4bc2d758db8143b8dd52c1491baed87c0bf925b8
Quelin <mateusz.janus@poczta.onet.pl>
2017-01-04 21:37:18 +0100

    USERS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,10 @@ class UsersController < ApplicationController
 
     def update
     @user = User.find(params[:id])
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+    params[:user].delete(:password)
+    params[:user].delete(:password_confirmation)
+    end
     if @user.update_attributes(user_params)
       redirect_to(:action => 'show', :id => @user.id)
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
          scope :firmy, -> { where(firma: true) }
          scope :osoby_prywatne, -> { where(firma: false) }
 
-         #validates :REGON, length: { is: 9 , message: "is a 9-digit code" }, if: :REGON.present?
-         #validates_presence_of :name, :lastname, :phone, :kod, :ulica, :miasto
-         #validates_numericality_of :phone, :NIP, :REGON
-
-         validates :NIP, length: { is: 10 }, if: 'NIP.present?'
+         validates :REGON, length: { is: 9 , message: "is a 9-digit code" }, allow_blank: true
+         validates_presence_of :name, :lastname, :phone, :kod, :ulica, :miasto
+         validates_numericality_of :phone
+		 validates_numericality_of :REGON, :NIP, allow_blank: true
+         validates :NIP, length: { is: 10 }, allow_blank: true
          
          
 
