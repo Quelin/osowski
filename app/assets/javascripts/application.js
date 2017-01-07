319e45adfc6fa90404fa5224f8c1a2a39b68c1b9
Quelin <mateusz.janus@poczta.onet.pl>
2017-01-07 17:54:57 +0100

    heroku1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require dataTables/jquery.dataTables
-//= require twitter/bootstrap
-//= require turbolinks
+//= require jquery 
+//= require jquery_ujs 
+//= require twitter/bootstrap 
+//= require dataTables/jquery.dataTables 
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap 
+//= require turbolinks 
 //= require_tree .
