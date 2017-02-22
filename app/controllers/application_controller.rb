b4a42d638cd21496d28a4c1d98a503824a22d273
Quelin <mateusz.janus@poczta.onet.pl>
2017-02-22 20:06:30 +0100

    -.-

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,7 @@ private
       "application"
     end
   end
+
+
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
  get '/services', to: 'services#index_all'
- root 'home#index'
+ root 'users#index'
 
 devise_for :users, controllers: {registrations: 'registrations'}, :path_prefix => 'my'
 
